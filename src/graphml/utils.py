614d5caad5ae32c7fe7bb55940cb1ad2398683fa
Federico Teotini <teotini.federico@gmail.com>
2020-05-15 15:53:04 +0200

    finished add_self_edges_to_adjacency_matrix

--- src/graphml/utils.py
+++ src/graphml/utils.py
@@ -3,8 +3,10 @@ import torch
 
 def add_self_edges_to_adjacency_matrix(adjency_coo_matrix):
     max_node_id = adjency_coo_matrix.max().item()
-    number_of_nodes = max_node_id + 1
+    self_edges = torch.arange(
+        max_node_id + 1, dtype=adjency_coo_matrix.dtype).repeat(2, 1)
 
-    self_edges = torch.arange(number_of_nodes).repeat(2, 1)
+    src_idxs, trg_idxs = adjency_coo_matrix
+    mask = src_idxs != trg_idxs
 
-    return torch.cat([adjency_coo_matrix, self_edges], dim=1)
+    return torch.cat([adjency_coo_matrix[:, mask], self_edges], dim=1)
--- tests/utils/add_self_edges_to_adjacency_matrix_test.py
+++ tests/utils/add_self_edges_to_adjacency_matrix_test.py
@@ -5,14 +5,35 @@ from graphml.utils import add_self_edges_to_adjacency_matrix
 def test_adding_self_edges_to_adjacency_coo_matrix():
     adj = torch.tensor([[0, 3, 4], [2, 2, 0]])
 
-
     result = add_self_edges_to_adjacency_matrix(adj)
 
     assert result.size(-1) == 8
-
     edge_list = result.t().tolist()
-    assert [0,0] in edge_list
-    assert [1,1] in edge_list
-    assert [2,2] in edge_list
-    assert [3,3] in edge_list
-    assert [4,4] in edge_list
+    assert [0, 0] in edge_list
+    assert [1, 1] in edge_list
+    assert [2, 2] in edge_list
+    assert [3, 3] in edge_list
+    assert [4, 4] in edge_list
+
+
+def test_adding_self_edge_to_an_adjacency_coo_matrix_already_containing_some_of_them():
+    adj = torch.tensor([[0, 1], [2, 1]])
+
+    result = add_self_edges_to_adjacency_matrix(adj).t().tolist()
+
+    assert len(result) == 4
+    assert [0, 0] in result
+    assert [1, 1] in result
+    assert [2, 2] in result
+    assert [0, 2] in result
+
+
+def test_the_resulting_tensor_should_have_the_same_properties_as_the_input():
+    adj = torch.tensor(
+        [[0, 1], [2, 1]], dtype=torch.float64, requires_grad=True)
+
+    result = add_self_edges_to_adjacency_matrix(adj)
+
+    assert result.dtype == torch.float64
+    assert result.device == adj.device
+    assert result.requires_grad == True
