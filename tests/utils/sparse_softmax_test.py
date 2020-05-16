5624e7e8ad14c2c4afc980cdf5ff93f6a23f2b5d
Federico Teotini <templarblack.fed03@gmail.com>
2020-05-16 19:16:45 +0200

    fixed dtype in test

--- tests/utils/sparse_softmax_test.py
+++ tests/utils/sparse_softmax_test.py
@@ -2,7 +2,7 @@ import torch
 from graphml.utils import sparse_softmax
 
 def test_sparse_softmax():
-    input = torch.tensor([45,2,9,90,20,90,8,8])
+    input = torch.tensor([45,2,9,90,20,90,8,8], dtype=torch.float)
     mask = torch.tensor([2,3,5,2,0,0,3,1])
 
     result = sparse_softmax(input, mask)
