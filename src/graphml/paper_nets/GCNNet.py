37386df0271be9f7fc98cefce16c38281b80b614
Federico Teotini <teotini.federico@gmail.com>
2020-06-21 11:39:59 +0200

    GCN net

---
+++ src/graphml/paper_nets/GCNNet.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn.functional as F
+from typing import Callable, Tuple
+from graphml.layers.gcn import GCNLayerFactory
+
+
+class GCNNet(torch.nn.Module):
+    def __init__(self, adjency_coo_matrix: torch.Tensor, input_feature_dim: torch.Size, number_of_classes: int):
+        super().__init__()
+
+        layer_factory = GCNLayerFactory(adjency_coo_matrix)
+        self._conv1 = layer_factory.create(input_feature_dim, 16)
+        self._conv2 = layer_factory.create(
+            16, number_of_classes, torch.nn.LogSoftmax(dim=1), .5)
+
+    def forward(self, input_matrix: torch.Tensor):
+        x = self._conv1(input_matrix)
+        x = self._conv2(x)
+        return x
+
+
+def loss_factory(train_mask: torch.Tensor) -> Callable[[torch.Tensor, torch.Tensor], torch.Tensor]:
+    def loss_func(model_output: torch.Tensor, labels: torch.Tensor):
+        if train_mask is None:
+            return F.nll_loss(model_output, labels)
+        else:
+            return F.nll_loss(model_output[train_mask], labels[train_mask])
+
+    return loss_func
+
+
+def GCN_model(
+    adjency_coo_matrix: torch.Tensor,
+    input_feature_dim: torch.Size,
+    number_of_classes: int,
+    train_mask: torch.Tensor = None,
+    learning_rate=.01,
+    weight_decay=5e-4
+) -> Tuple[GCNNet, Callable[[torch.Tensor, torch.Tensor], torch.Tensor], torch.optim.Optimizer]:
+    net = GCNNet(adjency_coo_matrix, input_feature_dim, number_of_classes)
+    return net, loss_factory(train_mask), torch.optim.Adam(net.parameters(), learning_rate)
---
+++ src/graphml/paper_nets/__init__.py
@@ -0,0 +1,1 @@
+from .GCNNet import GCN_model
