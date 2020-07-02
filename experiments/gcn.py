c82c8be2f29c796d0154848494153304b5811f8c
Federico Teotini <teotini.federico@gmail.com>
2020-07-02 17:33:57 +0200

    Save epoch stats to csv

--- experiments/gcn.py
+++ experiments/gcn.py
@@ -2,8 +2,10 @@ import os
 import math
 import torch
 from graphml.paper_nets import GCN_model
-from graphml.datasets.Cora import CoraDataset
+from graphml.datasets import CoraDataset, PubmedDataset
 from graphml.datasets.Transform import NormalizeFeatures
+from time import perf_counter
+import csv
 
 epochs = 200
 
@@ -12,8 +14,8 @@ model_file = os.path.join(current_file_directory, "best_gcn.pt")
 dev = torch.device(
     "cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-dataset = CoraDataset(current_file_directory, NormalizeFeatures())
-dataset.to(dev)
+dataset = PubmedDataset(current_file_directory, NormalizeFeatures()).load()
+dataset = dataset.to(dev)
 
 net, loss_fn, optimizer = GCN_model(
     dataset.adj_coo_matrix, dataset.features_per_node, dataset.number_of_classes)
@@ -29,7 +31,7 @@ def train():
     loss.backward()
     optimizer.step()
 
-    return loss
+    return loss.item()
 
 
 @torch.no_grad()
@@ -40,7 +42,7 @@ def evaluate():
     validation_accuracy = accuracy(
         output, dataset.labels, dataset.validation_mask)
     validation_loss = loss_fn(
-        output[dataset.validation_mask], dataset.labels[dataset.validation_mask])
+        output[dataset.validation_mask], dataset.labels[dataset.validation_mask]).item()
 
     return train_accuracy, validation_accuracy, validation_loss
 
@@ -53,7 +55,7 @@ def test():
     test_accuracy = accuracy(
         output, dataset.labels, dataset.test_mask)
     test_loss = loss_fn(
-        output[dataset.test_mask], dataset.labels[dataset.test_mask])
+        output[dataset.test_mask], dataset.labels[dataset.test_mask]).item()
 
     return test_accuracy, test_loss
 
@@ -65,12 +67,23 @@ def accuracy(logits, labels, mask):
     return acc
 
 
+epochs_stats = []
 best_val_loss = math.inf
 updated_best_loss_on_epoch = 0
 for epoch in range(epochs):
+    start = perf_counter()
     train_loss = train()
     train_accuracy, validation_accuracy, validation_loss = evaluate()
-    print(f"Epoch {epoch + 1}/{epochs} , Train Loss {train_loss:.4f} , Train Accuracy {train_accuracy:.4f} , Validation Loss {validation_loss:.4f} , Validation Accuracy {validation_accuracy:.4f}")
+    end = perf_counter()
+    print(f"Epoch {epoch + 1}/{epochs}, Train Loss {train_loss:.4f}, Train Accuracy {train_accuracy:.4f}, Validation Loss {validation_loss:.4f}, Validation Accuracy {validation_accuracy:.4f}, Time: {end-start:.5f}")
+    epochs_stats.append({
+        "epoch": epoch,
+        "train_loss": train_loss,
+        "train_accuracy": train_accuracy,
+        "validation_loss": validation_loss,
+        "validation_accuracy": validation_accuracy,
+        "time": end-start
+    })
     if validation_loss < best_val_loss:
         best_val_loss = validation_loss
         updated_best_loss_on_epoch = epoch
@@ -81,3 +94,8 @@ for epoch in range(epochs):
 
 test_accuracy, test_loss = test()
 print(f"Test Loss {test_loss:.4f} , Test Accuracy {test_accuracy:.4f}")
+
+with open(os.path.join(current_file_directory, "results.csv"), "w", newline="") as csv_file:
+    writer = csv.DictWriter(csv_file, epochs_stats[0].keys())
+    writer.writeheader()
+    writer.writerows(epochs_stats)
