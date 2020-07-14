59f903bd61cb46ffec835eeb7577f29237f54aea
Federico Teotini <teotini.federico@gmail.com>
2020-07-14 17:40:47 +0200

    Fixed transform param in Citeseer and Pubmed

--- src/graphml/datasets/CiteseerDataset.py
+++ src/graphml/datasets/CiteseerDataset.py
@@ -4,5 +4,5 @@ from .InternalData import InternalData
 
 
 class CiteseerDataset(PlanetoidDatasetLoader):
-    def __init__(self, base_path: str, transform: Callable[[InternalData], InternalData] = None):
-        super().__init__("citeseer", base_path, transform)
+    def __init__(self, base_path: str, *transform: Callable[[InternalData], InternalData]):
+        super().__init__("citeseer", base_path, *transform)
--- src/graphml/datasets/PubmedDataset.py
+++ src/graphml/datasets/PubmedDataset.py
@@ -4,5 +4,5 @@ from .InternalData import InternalData
 
 
 class PubmedDataset(PlanetoidDatasetLoader):
-    def __init__(self, base_path: str, transform: Callable[[InternalData], InternalData] = None):
-        super().__init__("pubmed", base_path, transform)
+    def __init__(self, base_path: str, *transform: Callable[[InternalData], InternalData]):
+        super().__init__("pubmed", base_path, *transform)
