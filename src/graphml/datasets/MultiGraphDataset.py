7157b8da5e45973082c0b08c4e06b167b1dcfe2c
Federico Teotini <teotini.federico@gmail.com>
2020-07-19 18:24:43 +0200

    MultiGraphDataset

---
+++ src/graphml/datasets/MultiGraphDataset.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List
+from .InternalData import InternalData
+
+
+@dataclass
+class MultiGraphDataset():
+    train: List[InternalData]
+    validation: List[InternalData]
+    test: List[InternalData]
+    features_per_node: int
+    number_of_classes: int
+
+    def to(self, *args, **kwargs):
+        self.train = list(
+            map(lambda data: data.to(*args, **kwargs), self.train))
+        self.validation = list(
+            map(lambda data: data.to(*args, **kwargs), self.validation))
+        self.test = list(
+            map(lambda data: data.to(*args, **kwargs), self.test))
+        
+        return self
