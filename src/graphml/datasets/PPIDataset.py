0e50b56517b2514e945125b5eb80fdaa9626d42c
Federico Teotini <teotini.federico@gmail.com>
2020-07-19 18:25:26 +0200

    PPI dataset uses MultiGraph

--- src/graphml/datasets/PPIDataset.py
+++ src/graphml/datasets/PPIDataset.py
@@ -5,8 +5,9 @@ import zipfile
 import numpy as np
 from networkx import DiGraph
 from networkx.readwrite import json_graph
+from graphml.datasets.MultiGraphDataset import MultiGraphDataset
 from graphml.utils import remove_self_loops
-from typing import Callable, Dict, List, Union
+from typing import Callable, List, Union
 from graphml.datasets.InternalData import InternalData
 from graphml.datasets.BaseDatasetLoader import BaseDatasetLoader
 
@@ -28,8 +29,13 @@ class PPIDataset(BaseDatasetLoader):
     def _split_raw_file(self, split: str, raw_name: str) -> str:
         return os.path.join(self._raw_folder, f"{split}_{raw_name}")
 
-    def _process_raw_files(self) -> Dict[str, List[InternalData]]:
-        return {split: self._process_split(split) for split in self.splits}
+    def _process_raw_files(self) -> MultiGraphDataset:
+        split_data = {split: self._process_split(
+            split) for split in self.splits}
+        num_of_classes = split_data["train"][0].number_of_classes
+        features_per_node = split_data["train"][0].features_per_node
+
+        return MultiGraphDataset(split_data["train"], split_data["valid"], split_data["test"], features_per_node, num_of_classes)
 
     def _process_split(self, split: str) -> List[InternalData]:
         with open(self._split_raw_file(split, "graph.json"), 'r') as f:
@@ -39,7 +45,8 @@ class PPIDataset(BaseDatasetLoader):
         x = torch.from_numpy(x).to(torch.float)
 
         y = np.load(self._split_raw_file(split, "labels.npy"))
-        y = torch.from_numpy(y).to(torch.float)
+        y = torch.from_numpy(y).to(torch.float).argmax(
+            dim=1)  # one hot to enumerate
 
         graphs_idxs = np.load(self._split_raw_file(split, "graph_id.npy"))
         graphs_idxs = torch.from_numpy(graphs_idxs).to(torch.long)
@@ -66,7 +73,7 @@ class PPIDataset(BaseDatasetLoader):
         return adj
 
     def _dowload_file(self, url: str, file_name: str):
-        super()._dowload_file(url,file_name)
+        super()._dowload_file(url, file_name)
 
         raw_file = os.path.join(self._raw_folder, self._raw_file_names)
         with zipfile.ZipFile(raw_file, 'r') as f:
