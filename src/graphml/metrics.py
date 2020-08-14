46ec74dc1e0cbbd648b8415d7d439392365ccd45
Federico Teotini <teotini.federico@gmail.com>
2020-08-14 03:45:18 +0200

    Created MicroF1 metric

--- src/graphml/metrics.py
+++ src/graphml/metrics.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Callable
+from sklearn.metrics import f1_score
+
+import torch
+
+
+""" class MetricCalculator(ABC):
+    @abstractmethod
+    def calc(self, prefix: str, logits: torch.Tensor, labels: torch.Tensor) -> Metric:
+        pass """
 
 
 @dataclass
@@ -18,11 +28,50 @@ class Metric(ABC):
 
 @dataclass
 class Loss(Metric):
-    def better_then(self, other: Metric):
+    def better_then(self, other: Loss):
         return True if other is None else self.value <= other.value
+
+    """ class Calculator(MetricCalculator):
+        def __init__(self, loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor]):
+            self._loss_fn = loss_fn
+
+        def calc(self, name: str, logits: torch.Tensor, labels: torch.Tensor) -> Metric:
+            return Loss(f"{name}_loss", self._loss_fn(logits, labels).item()) """
 
 
 @dataclass
 class Accuracy(Metric):
-    def better_then(self, other: Metric):
+    def better_then(self, other: Accuracy):
         return True if other is None else self.value >= other.value
+
+    """ class Calculator(MetricCalculator):
+        def calc(self, name: str, logits: torch.Tensor, labels: torch.Tensor) -> Metric:
+            return Accuracy(f"{name}_loss", self.accuracy(logits, labels))
+
+        @staticmethod
+        def accuracy(logits: torch.Tensor, labels: torch.Tensor) -> float:
+            assert len(logits) == len(labels)
+            pred = logits.argmax(dim=1)
+            correct_pred_number = torch.eq(pred, labels).sum().item()
+            acc = correct_pred_number / len(labels)
+            return acc """
+
+
+@dataclass
+class MicroF1(Metric):
+    def better_then(self, other: MicroF1):
+        return True if other is None else self.value >= other.value
+
+    @staticmethod
+    def calc(logits: torch.Tensor, labels: torch.Tensor) -> float:
+        pred = logits.sigmoid().round()
+        return f1_score(labels, pred, average='micro')
+
+    """ class Calculator(MetricCalculator):
+        def calc(self, name: str, logits: torch.Tensor, labels: torch.Tensor) -> Metric:
+            return MicroF1(f"{name}_loss", self.micro_f1(logits, labels))
+
+        @staticmethod
+        def micro_f1(logits: torch.Tensor, labels: torch.Tensor) -> float:
+            pred = logits.sigmoid().round()
+            return f1_score(labels, pred, average='micro') """
