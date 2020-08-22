9e2b4c2dc472c883b4a27396a814ffd854e78cf1
Federico Teotini <teotini@localhost.dinfo.unifi.it>
2020-08-22 16:32:03 +0200

    MiniBatchLoader performance

--- src/graphml/MiniBatchLoader.py
+++ src/graphml/MiniBatchLoader.py
@@ -1,7 +1,6 @@
-from time import perf_counter
-from typing import List, Optional, Union, NamedTuple
+from typing import List, NamedTuple
 import torch
-from torch import dtype
+import numpy as np
 from torch.utils.data import DataLoader
 from graphml.utils import sample_neighbors
 
@@ -21,14 +20,25 @@ class SampledAdjacency(NamedTuple):
             self.original_sampled_adj.to(*args, **kwargs)
         ) """
 
-def isin(test:torch.Tensor, trg: torch.Tensor) -> torch.Tensor:
-    trg_uq, uq_idx = torch.unique(trg,return_inverse=True)
+
+def stable_argsort(arr, dim=-1, descending=False):
+    arr_np = arr.detach().cpu().numpy()
+    if descending:
+        indices = np.argsort(-arr_np, axis=dim, kind='stable')
+    else:
+        indices = np.argsort(arr_np, axis=dim, kind='stable')
+    return torch.from_numpy(indices).long().to(arr.device)
+
+
+def isin(test: torch.Tensor, trg: torch.Tensor) -> torch.Tensor:
+    trg_uq, uq_idx = torch.unique(trg, return_inverse=True)
 
     tmp = torch.cat([trg_uq, test])
-    tmp_order = torch.argsort(tmp)
+    tmp_order = stable_argsort(tmp)
 
     tmp_sorted = tmp[tmp_order]
-    flag = torch.cat([tmp_sorted[1:] == tmp_sorted[:-1],torch.tensor([False], dtype=torch.bool, device=trg.device)])
+    flag = torch.cat([tmp_sorted[1:] == tmp_sorted[:-1],
+                      torch.tensor([False], dtype=torch.bool, device=trg.device)])
 
     mask = torch.empty_like(tmp, dtype=torch.bool)
     mask[tmp_order] = flag
@@ -36,10 +46,25 @@ def isin(test:torch.Tensor, trg: torch.Tensor) -> torch.Tensor:
     return mask[uq_idx]
 
 
+def map_tensor_to_new_idxs(trg: torch.Tensor, old_idxs: torch.Tensor) -> torch.Tensor:
+    assert trg.dim() == 1 or trg.dim() == 2
+
+    vectors = [trg] if trg.dim() == 1 else [v for v in trg]
+    new_idxs = torch.arange(len(old_idxs), device=old_idxs.device)
+
+    for vec in vectors:
+        order = torch.argsort(vec)
+        uq, rev_idx = vec[order].unique_consecutive(return_inverse=True)
+        mask = isin(uq, old_idxs)
+        vec[order] = (new_idxs[mask])[rev_idx]
+    return torch.stack(vectors) if len(vectors) > 1 else vectors[0]
+
+
 class BatchStep(NamedTuple):
     target_idxs: torch.Tensor
     node_idxs: torch.Tensor
     sampled_adj: List[torch.Tensor]
+
 
 class MiniBatchLoader(DataLoader):
     def __init__(self, adjacency_coo_matrix: torch.Tensor, neighborhood_sizes: List[int], node_mask: torch.Tensor = None, **kwargs):
@@ -60,32 +85,25 @@ class MiniBatchLoader(DataLoader):
                          collate_fn=self._sample_needed_neighbors, **kwargs)
 
     def _sample_needed_neighbors(self, node_idxs_batch: List[int]):
-        s = perf_counter()
         trg_idx = torch.tensor(node_idxs_batch, device=self._adj.device)
-        
+
         batch_idxs = trg_idx.clone()
         adjs = []
         for step_size in reversed(self._neighborhood_sizes):
             step_adj = self._select_adj_by_src_idxs(batch_idxs)
-            step_adj = sample_neighbors(step_adj,step_size)
+            if step_size != -1:
+                step_adj = sample_neighbors(step_adj, step_size) 
             adjs.append(step_adj)
-            batch_idxs = torch.unique(step_adj)
+            batch_idxs = torch.unique(step_adj, sorted=True)
 
-        adjs = [self._map_adjs_to_new_idxs(batch_idxs, adj) for adj in adjs]
+        adjs = [map_tensor_to_new_idxs(adj, batch_idxs) for adj in adjs]
 
-        l= trg_idx,batch_idxs,self._map_adjs_to_new_idxs(batch_idxs, trg_idx.clone()),reversed(adjs)
-        print(f"mini {(perf_counter() -s)}")
+        l = trg_idx, batch_idxs, map_tensor_to_new_idxs(
+            trg_idx.clone(), batch_idxs), reversed(adjs)
         return l
 
     def _select_adj_by_src_idxs(self, src_idxs: List[int]) -> torch.Tensor:
-        mask = isin(src_idxs,self._adj[0])
+        mask = isin(src_idxs, self._adj[0])
 
-        l = self._adj[:,mask]
+        l = self._adj[:, mask]
         return l
-
-    def _map_adjs_to_new_idxs(self, node_idxs: torch.Tensor, adj: torch.Tensor) -> torch.Tensor:
-        for new_idx, old_idx in enumerate(node_idxs):
-            adj[adj == old_idx] = new_idx
-
-        # return SampledAdjacency(sampled, adj)
-        return adj
